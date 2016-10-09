43dde6d6aa339177ae24367f636c89d06c41054b
progre <djyayutto@gmail.com>
2016-10-09 18:43:15 +0900

    Fix exit when window closed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /// <reference path="../typings/index.d.ts" />
 try { require("source-map-support").install(); } catch (e) { /* empty */ }
 const log4js = require("log4js");
-import {app, BrowserWindow} from "electron";
+import { app, BrowserWindow } from "electron";
 
 log4js.configure({
     appenders: [{ type: "console", layout: { type: "basic" } }]
@@ -9,6 +9,11 @@ log4js.configure({
 
 async function main() {
     await new Promise((resolve, reject) => app.once("ready", resolve));
+
+    app.on("window-all-closed", () => {
+        app.quit()
+    });
+
     let win = new BrowserWindow({
         width: 800,
         height: 600,
